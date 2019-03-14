205ad92376b4922e8400cea1d50e8e7389ff0029
Anas Morsi <anasmorsi@gmail.com>
2019-03-14 11:55:34 -0400

    Javascript

---
+++ elsir-part-two.js
@@ -0,0 +1,19 @@
+let inputTemp = 75;
+
+let tempFahr = inputTemp;
+
+if(tempFahr <54){
+
+tempFahr='a coat';
+
+} else if(53 < tempFahr && tempFahr < 71){
+
+tempFahr='a jacket';
+
+}else{
+
+tempFahr='no jacket';
+
+}
+
+console.log(`Since it is ${inputTemp} degrees and you are going to a ${inputEvent}, you should wear ${eventType} and ${tempFahr}.`);
