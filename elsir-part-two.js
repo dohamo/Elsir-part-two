85a1ff5e7600717e0b4dc6b53f0d967021e59ef3
Doha Elsir <47755297+dohamo@users.noreply.github.com>
2019-03-15 20:52:15 -0400

    Update elsir-part-two.js

--- elsir-part-two.js
+++ elsir-part-two.js
@@ -1,19 +1,20 @@
 let inputTemp = 75;
-
 let tempFahr = inputTemp;
-
 if(tempFahr <54){
-
 tempFahr='a coat';
-
 } else if(53 < tempFahr && tempFahr < 71){
-
 tempFahr='a jacket';
-
 }else{
-
 tempFahr='no jacket';
-
 }
 
+let inputTemp = 70;
+var tempFahr = inputTemp;
+if(tempFahr <54){
+  tempFahr = 'a coat';
+  } else if(53 < tempFahr && tempFahr < 71){
+    tempFahr='a jacket';
+}else{
+  tempFahr='no jacket';
+}
 console.log(`Since it is ${inputTemp} degrees and you are going to a ${inputEvent}, you should wear ${eventType} and ${tempFahr}.`);
