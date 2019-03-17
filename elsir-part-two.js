3345439e63744f54aba5676d517affad64e88bd0
Doha Elsir <47755297+dohamo@users.noreply.github.com>
2019-03-17 16:51:58 -0400

    Update elsir-part-two.js

--- elsir-part-two.js
+++ elsir-part-two.js
@@ -1,20 +1,27 @@
-let inputTemp = 75;
-let tempFahr = inputTemp;
-if(tempFahr <54){
-tempFahr='a coat';
-} else if(53 < tempFahr && tempFahr < 71){
-tempFahr='a jacket';
-}else{
-tempFahr='no jacket';
+let inputEvent = prompt('What type of event are you attending?', 'casual/semi-formal/formal');
+var eventType = inputEvent;
+switch (eventType) {
+  case 'casual':
+    eventType = 'something comfy';
+    break;
+  case 'semi-formal':
+    eventType = 'a polo';
+    break;
+  case 'formal':
+    eventType = 'a suit';
+    break;
+  default:
+    eventType = 'anything you like';
+    break;
 }
-
-let inputTemp = 70;
+let inputTemp = prompt('What is the temperature outside?', 'degrees Fahrenheit');
 var tempFahr = inputTemp;
-if(tempFahr <54){
+if (tempFahr < 54) {
   tempFahr = 'a coat';
-  } else if(53 < tempFahr && tempFahr < 71){
-    tempFahr='a jacket';
-}else{
-  tempFahr='no jacket';
+} else if (53 < tempFahr && tempFahr < 71) {
+  tempFahr = 'a jacket';
+} else {
+  tempFahr = 'no jacket';
 }
-console.log(`Since it is ${inputTemp} degrees and you are going to a ${inputEvent}, you should wear ${eventType} and ${tempFahr}.`);
+console.log(`Since it is ${inputTemp} degrees Fahrenheit and you are going to a ${inputEvent} event, you should wear ${eventType} and ${tempFahr}.`);
+
